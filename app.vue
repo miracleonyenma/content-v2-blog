c1ceb95a2f9049d08dfed833d3a5b5d44cab2dc8
Miracle Onyenma <42788603+miracleonyenma@users.noreply.github.com>
2023-03-09 09:44:25 +0000

    update: add simple meta

--- app.vue
+++ app.vue
@@ -1,4 +1,12 @@
 <!-- ./app.vue -->
+<script setup>
+useHead({
+  title: "Nuxt 3 + Content v2 Starter",
+  meta: [
+    { name: 'description', content: 'Nuxt 3 + Content v2 Starter built by Miracleio' }
+  ],
+})
+</script>
 <template>
   <div>
     <SiteHeader />
